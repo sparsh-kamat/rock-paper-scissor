79329b6c5a0ba3e21fcab6ccd0369c4143843fbb
sparsh-kamat <kamatsparsh@gmail.com>
2022-06-15 16:19:35 +0530

    Deleted everything and doing gui from screatch

--- index.js
+++ index.js
@@ -1,46 +1,15 @@
 function computerPlay() {
-    let rand = Math.floor(Math.random()*3);
-    return (rand == 0)? 'Rock': (rand == 1)? 'Paper': 'Scissors';
+  let rand = Math.floor(Math.random() * 3);
+  switch (rand) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "SCISSORS";
+  }
 }
 
-function compare(a, b) {
-    if (a === 'rock') {
-        if (b === 'scissors') return 1;
-        else if (b === 'paper') return -1;
-        else return 0;
-    } else if (a === 'paper') {
-        if (b === 'scissors') return -1;
-        else if (b === 'rock') return 1;
-        else return 0;
-    } else {
-        if (b === 'paper') return 1;
-        else if (b === 'rock') return -1;
-        else return 0;
-    }
-}
-
-let format = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-
-function playRound(player, computer) {
-    let result = compare(player.toLowerCase(), computer.toLowerCase());
-    if (result) return `It is a Tie! You both played ${format(player)}!`;
-    else if (result) return `You Win! ${format(player)} beats ${format(computer)}`;
-    else return `You Lose! ${format(computer)} beats ${format(player)}`;
-}
-
-function game() {
-    for (i = 0; i < 5; i++) {
-        let player = prompt(`Round ${i+1}! You will play... (Rock, Paper, Scissors)`);
-        player = player.toLowerCase();
-        if (player !== "rock" && player !== "paper" && player !== "scissors") {
-            alert("Invalid choice, enter Rock, Paper, or Scissors only!");
-            i--;
-        } else {
-            let result = playRound(player, computerPlay())
-            console.log(result);
-            alert(result);
-        }
-    }
-}
-
-game();
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
